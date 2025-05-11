ba43c4affd84de06d883467d93b2a567b531cd34
RangiraDave <rangiradev666@gmail.com>
2025-05-11 14:50:48 +0200

    Nullable contact column

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ python3 manage.py collectstatic --no-input
 echo "Running migrations..."
 
 # First, ensure core app is installed
-python3 manage.py makemigrations core 0002 --fake --no-input
-python3 manage.py migrate --no-input
+python3 manage.py makemigrations core --no-input
+python3 manage.py migrate 0002 --fake --no-input
 
 # Remove contact column if it exists (PostgreSQL command)
 # psql -c "ALTER TABLE core_product DROP COLUMN IF EXISTS contact;"
