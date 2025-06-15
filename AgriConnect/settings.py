1a2afef312ac045bf429840a0d17273e4546db3a
RangiraDave <rangiradev666@gmail.com>
2025-06-15 12:57:45 +0200

    Set debug to false

--- AgriConnect/settings.py
+++ AgriConnect/settings.py
@@ -201,9 +201,8 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 # Django REST Framework configuration
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
